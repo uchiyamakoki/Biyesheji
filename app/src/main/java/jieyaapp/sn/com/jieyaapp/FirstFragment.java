237dd8ed6f896a7ff302de5ad8030fd575f43d09
momoka <454768497@qq.com>
2018-01-15 20:58:14 +0800

    底部导航栏完成

--- app/src/main/java/jieyaapp/sn/com/jieyaapp/FirstFragment.java
+++ app/src/main/java/jieyaapp/sn/com/jieyaapp/FirstFragment.java
@@ -2,6 +2,7 @@ package jieyaapp.sn.com.jieyaapp;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +15,16 @@ public class FirstFragment extends Fragment {
     private String context;
     private TextView mTextView;
 
+    public FirstFragment(String context){
+        this.context = context;
+    }
+
+    @Nullable
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view=inflater.inflate(R.layout.first_fragment,container,false);
         mTextView =(TextView)view.findViewById(R.id.txt_content);
+        mTextView.setText(context);
         return view;
     }
 }
--- app/src/main/java/jieyaapp/sn/com/jieyaapp/MainActivity.java
+++ app/src/main/java/jieyaapp/sn/com/jieyaapp/MainActivity.java
@@ -1,6 +1,8 @@
 package jieyaapp.sn.com.jieyaapp;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,8 +12,21 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    private TextView topBar;
+    private TextView tabDeal;
+    private TextView tabPoi;
+    private TextView tabMore;
+    private TextView tabUser;
+
+    private FrameLayout ly_content;
+
+    private FirstFragment f1,f2,f3,f4;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,95 @@ public class MainActivity extends Activity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
         setContentView(R.layout.activity_main);
 
+        bindView();
+    }
+    //UI组件初始化与事件绑定
+    private void bindView() {
+        topBar = (TextView)this.findViewById(R.id.txt_top);
+        tabDeal = (TextView)this.findViewById(R.id.txt_deal);
+        tabPoi = (TextView)this.findViewById(R.id.txt_poi);
+        tabUser = (TextView)this.findViewById(R.id.txt_user);
+        tabMore = (TextView)this.findViewById(R.id.txt_more);
+        ly_content = (FrameLayout) findViewById(R.id.fragment_container);
+
+        tabDeal.setOnClickListener(this);
+        tabMore.setOnClickListener(this);
+        tabUser.setOnClickListener(this);
+        tabPoi.setOnClickListener(this);
+
+    }
+    //重置所有文本的选中状态
+    public void selected(){
+        tabDeal.setSelected(false);
+        tabMore.setSelected(false);
+        tabPoi.setSelected(false);
+        tabUser.setSelected(false);
+    }
+    //隐藏所有Fragment
+    public void hideAllFragment(FragmentTransaction transaction){
+        if(f1!=null){
+            transaction.hide(f1);
+        }
+        if(f2!=null){
+            transaction.hide(f2);
+        }
+        if(f3!=null){
+            transaction.hide(f3);
+        }
+        if(f4!=null){
+            transaction.hide(f4);
+        }
+    }
+    @Override
+    public void onClick(View v) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        hideAllFragment(transaction);
+        switch(v.getId()){
+            case R.id.txt_deal:
+                selected();
+                tabDeal.setSelected(true);
+                if(f1==null){
+                    f1 = new FirstFragment("第一个Fragment");
+                    transaction.add(R.id.fragment_container,f1);
+                }else{
+                    transaction.show(f1);
+                }
+                break;
+
+            case R.id.txt_more:
+                selected();
+                tabMore.setSelected(true);
+                if(f2==null){
+                    f2 = new FirstFragment("第二个Fragment");
+                    transaction.add(R.id.fragment_container,f2);
+                }else{
+                    transaction.show(f2);
+                }
+                break;
+
+            case R.id.txt_poi:
+                selected();
+                tabPoi.setSelected(true);
+                if(f3==null){
+                    f3 = new FirstFragment("第三个Fragment");
+                    transaction.add(R.id.fragment_container,f3);
+                }else{
+                    transaction.show(f3);
+                }
+                break;
+
+            case R.id.txt_user:
+                selected();
+                tabUser.setSelected(true);
+                if(f4==null){
+                    f4 = new FirstFragment("第四个Fragment");
+                    transaction.add(R.id.fragment_container,f4);
+                }else{
+                    transaction.show(f4);
+                }
+                break;
+        }
+
+        transaction.commit();
     }
 }
