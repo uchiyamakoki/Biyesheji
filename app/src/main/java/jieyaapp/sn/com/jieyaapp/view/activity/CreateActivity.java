2c71eb7efe1c29760a6d831ecf43ceb8e5fdca80
momoka <454768497@qq.com>
2018-01-19 19:50:51 +0800

    一周的计划

---
+++ app/src/main/java/jieyaapp/sn/com/jieyaapp/view/activity/CreateActivity.java
@@ -0,0 +1,19 @@
+package jieyaapp.sn.com.jieyaapp.view.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import jieyaapp.sn.com.jieyaapp.R;
+
+/**
+ * Created by John on 2018/1/19.
+ */
+public class CreateActivity extends Activity {
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+
+    }
+
+}
--- app/src/main/java/jieyaapp/sn/com/jieyaapp/view/fragment/DealFragment.java
+++ app/src/main/java/jieyaapp/sn/com/jieyaapp/view/fragment/DealFragment.java
@@ -1,6 +1,7 @@
 package jieyaapp.sn.com.jieyaapp.view.fragment;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import jieyaapp.sn.com.jieyaapp.R;
+import jieyaapp.sn.com.jieyaapp.view.activity.CreateActivity;
+import jieyaapp.sn.com.jieyaapp.view.activity.MainActivity;
 
 /**
  * Created by John on 2018/1/18.
@@ -18,6 +21,13 @@ public class DealFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view=inflater.inflate(R.layout.deal_fragment,container,false);
         dButton=(Button) view.findViewById(R.id.dealbutton1);
+        dButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), CreateActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
