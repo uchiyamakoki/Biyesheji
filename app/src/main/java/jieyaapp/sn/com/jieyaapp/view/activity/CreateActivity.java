0538b6f6d91021b2ee6bfdbb4b873a19a4a37f43
momoka <454768497@qq.com>
2018-02-04 17:17:41 +0800

    将question view引入布局

--- app/src/main/java/jieyaapp/sn/com/jieyaapp/view/activity/CreateActivity.java
+++ app/src/main/java/jieyaapp/sn/com/jieyaapp/view/activity/CreateActivity.java
@@ -1,8 +1,12 @@
 package jieyaapp.sn.com.jieyaapp.view.activity;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
 
 import jieyaapp.sn.com.jieyaapp.R;
 
@@ -13,8 +17,22 @@ public class CreateActivity extends Activity {
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        translucentStatusBar();
         setContentView(R.layout.activity_create);
 
+    }
+
+    private void translucentStatusBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0及以上
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {//4.4到5.0
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+        }
     }
 
 }
